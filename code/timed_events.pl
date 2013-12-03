2ee569914e976d56f383b7d45138cb3e292d4a33
Douglas Parrish <douglas_parrish@dell.com>
2013-12-03 14:46:19 -0600

    perltidy cleanups

--- code/timed_events.pl
+++ code/timed_events.pl
@@ -1,9 +1,9 @@
 
-if (time_now "$Time_Sunset - 1:00") {
+if ( time_now "$Time_Sunset - 1:00" ) {
     set $Outside_Lights ON;
     run_voice_cmd "Normal Lights";
 }
 
-if (time_now "$Time_Sunrise_Twilight") {
+if ( time_now "$Time_Sunrise_Twilight" ) {
     set $Outside_Lights OFF;
 }
--- code/voice_commands.pl
+++ code/voice_commands.pl
@@ -1,50 +1,51 @@
 
-
-$basic_lights = new Voice_Cmd('Basic Lights');
-$goodnight = new Voice_Cmd('Goodnight Misterhouse');
-$inside_lights = new Voice_Cmd('Inside Lights [on,off]');
+$basic_lights   = new Voice_Cmd('Basic Lights');
+$goodnight      = new Voice_Cmd('Goodnight Misterhouse');
+$inside_lights  = new Voice_Cmd('Inside Lights [on,off]');
 $outside_lights = new Voice_Cmd('Outside Lights [on,off]');
-$normal_lights = new Voice_Cmd('Normal Lights');
+$normal_lights  = new Voice_Cmd('Normal Lights');
 
-if (said $normal_lights) {
-    my $all = new Group(list $Inside_Lights);
+if ( said $normal_lights) {
+    my $all = new Group( list $Inside_Lights);
 
     set $bookshelf_light ON;
-    $all -> remove ($bookshelf_light);
+    $all->remove($bookshelf_light);
     set $kitchen_over_cabinet "80%";
-    $all -> remove ($kitchen_over_cabinet);
+    $all->remove($kitchen_over_cabinet);
     set $dining_room "80%";
-    $all -> remove ($dining_room);
+    $all->remove($dining_room);
     set $foyer_light "40%";
-    $all -> remove ($foyer_light);
+    $all->remove($foyer_light);
     set $hallway_south "40%";
-    $all -> remove ($hallway_south);
+    $all->remove($hallway_south);
     set $dougs_lamp "60%";
-    $all -> remove ($dougs_lamp);
+    $all->remove($dougs_lamp);
     set $summers_lamp "60%";
-    $all -> remove ($summers_lamp);
+    $all->remove($summers_lamp);
 
     set $all OFF;
 }
 
-if (said $goodnight) {
-    my $all = new Group(list $Inside_Lights);
-    $all -> remove ( $dougs_lamp );
-    set $all OFF
+if ( said $goodnight) {
+    my $all = new Group( list $Inside_Lights);
+    $all->remove($dougs_lamp);
+    set $all OFF;
 }
 
-if ($state = said $inside_lights) {
-    if ($state eq 'off') {
+if ( $state = said $inside_lights) {
+    if ( $state eq 'off' ) {
         set $Inside_Lights OFF;
-    } else {
+    }
+    else {
         set $Inside_Lights ON;
     }
 }
 
-if ($state = said $outside_lights) {
-    if ($state eq 'off') {
+if ( $state = said $outside_lights) {
+    if ( $state eq 'off' ) {
         set $Outside_Lights OFF;
-    } else {
+    }
+    else {
         set $Outside_Lights ON;
     }
 }
